f933f1ae53bdf5bf6d4eb139d67c941a1920a045
Luke Cheng <Lukeecheng@gmail.com>
2023-06-07 20:38:56 -0700

    Documented our program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+/** An example program
+ *
+ * @author Luke Cheng
+ * @version 1.0
+ */
 public class Main
 {
+    /**
+     * Entry point for the
+     * @param args comman Line args
+     */
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
